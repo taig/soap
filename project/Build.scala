650c7c4f934e82a92b92d7c3c1a765047edd09ed
Niklas Klein <mail@taig.io>
2015-05-31 17:12:15 +0200

    Prepare next development iteration

--- project/Build.scala
+++ project/Build.scala
@@ -33,7 +33,7 @@ extends	android.AutoBuild
 			),
 			scalaVersion := "2.11.6",
 			scalacOptions ++= Seq( "-deprecation", "-feature" ),
-			version := "1.2.6"
+			version := "2.0.0-SNAPSHOT"
 		)
 		.settings(
 			minSdkVersion in Android := "4",
