6466f6be65d5ae354542d9e802efe910a9dabec2
Niklas Klein <mail@taig.io>
2015-09-11 02:30:28 +0200

    Move sonatype settings into sonatype.sbt

--- project/Build.scala
+++ project/Build.scala
@@ -8,7 +8,7 @@ import xerial.sbt.Sonatype.SonatypeKeys._
 object	Build
 extends	sbt.Build
 {
-	lazy val main = Project( "parcelable", file( "." ), settings = sonatypeSettings )
+	lazy val main = Project( "parcelable", file( "." ) )
 		.settings(
 			fork in Test := true,
 			javacOptions ++= (
@@ -39,51 +39,5 @@ extends	sbt.Build
 				Nil
 			),
 			version := "2.4.0-SNAPSHOT"
-		)
-		.settings(
-			description := "Parcelable compile time code generation for Scala on Android",
-			homepage := Some( url( "https://github.com/taig/parcelable" ) ),
-			licenses := Seq( "MIT" -> url( "https://raw.githubusercontent.com/taig/parcelable/master/LICENSE" ) ),
-			organizationHomepage := Some( url( "http://taig.io" ) ),
-			pomExtra :=
-			{
-				<issueManagement>
-					<url>https://github.com/taig/parcelable/issues</url>
-					<system>GitHub Issues</system>
-				</issueManagement>
-				<developers>
-					<developer>
-						<id>Taig</id>
-						<name>Niklas Klein</name>
-						<email>mail@taig.io</email>
-						<url>http://taig.io/</url>
-					</developer>
-				</developers>
-			},
-			pomIncludeRepository := { _ => false },
-			publishArtifact in Test := false,
-			publishMavenStyle := true,
-			publishTo <<= version ( version =>
-			{
-				val url = Some( "https://oss.sonatype.org/" )
-
-				if( version.endsWith( "SNAPSHOT" ) )
-				{
-					url.map( "snapshot" at _ + "content/repositories/snapshots" )
-				}
-				else
-				{
-					url.map( "release" at _ + "service/local/staging/deploy/maven2" )
-				}
-			} ),
-			scmInfo := Some(
-				ScmInfo(
-					url( "https://github.com/taig/parcelable" ),
-					"scm:git:git://github.com/taig/parcelable.git",
-					Some( "scm:git:git@github.com:taig/parcelable.git" )
-				)
-			),
-			sonatypeProfileName := "io.taig",
-			startYear := Some( 2015 )
 		)
 }
