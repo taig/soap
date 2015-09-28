43a3d5ec0f2a7d888ca8b96fd96685a70993c45e
Niklas Klein <mail@taig.io>
2015-09-29 01:14:16 +0200

    Add Intent support

--- src/main/scala/io/taig/android/parcelable/package.scala
+++ src/main/scala/io/taig/android/parcelable/package.scala
@@ -1,11 +1,24 @@
 package io.taig.android
 
 import _root_.android.os.Bundle
+import android.content.Intent
 
 package object parcelable {
-    implicit class RichBundle( bundle: Bundle ) {
+    implicit class ParcelableBundle( bundle: Bundle ) {
         def read[T: Bundleize]( key: String ) = implicitly[Bundleize[T]].read( key, bundle )
 
         def write[T: Bundleize]( key: String, value: T ) = implicitly[Bundleize[T]].write( key, value, bundle )
+    }
+
+    implicit class ParcelableIntent( intent: Intent ) {
+        def read[T: Bundleize]( key: String ) = intent.getExtras.read[T]( key )
+
+        def write[T: Bundleize]( key: String, value: T ) = {
+            if ( intent.getExtras == null ) {
+                intent.putExtras( new Bundle() )
+            }
+
+            intent.getExtras.write( key, value )
+        }
     }
 }
