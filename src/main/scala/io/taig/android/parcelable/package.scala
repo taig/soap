9cd1b0dc34779a33afc83ae3057a8759b9071905
Niklas Klein <mail@taig.io>
2015-09-29 02:05:29 +0200

    Make Intent & Bundle writes chainable

--- src/main/scala/io/taig/android/parcelable/package.scala
+++ src/main/scala/io/taig/android/parcelable/package.scala
@@ -4,21 +4,25 @@ import _root_.android.os.Bundle
 import android.content.Intent
 
 package object parcelable {
-    implicit class ParcelableBundle( bundle: Bundle ) {
+    implicit class ParcelableBundle( val bundle: Bundle ) {
         def read[T: Bundleize]( key: String ) = implicitly[Bundleize[T]].read( key, bundle )
 
-        def write[T: Bundleize]( key: String, value: T ) = implicitly[Bundleize[T]].write( key, value, bundle )
+        def write[T: Bundleize]( key: String, value: T ): bundle.type = {
+            implicitly[Bundleize[T]].write( key, value, bundle )
+            bundle
+        }
     }
 
-    implicit class ParcelableIntent( intent: Intent ) {
+    implicit class ParcelableIntent( val intent: Intent ) {
         def read[T: Bundleize]( key: String ) = intent.getExtras.read[T]( key )
 
-        def write[T: Bundleize]( key: String, value: T ) = {
+        def write[T: Bundleize]( key: String, value: T ): intent.type = {
             if ( intent.getExtras == null ) {
                 intent.putExtras( new Bundle() )
             }
 
             intent.getExtras.write( key, value )
+            intent
         }
     }
 }
